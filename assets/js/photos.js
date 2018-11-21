3c6de9c89d1a968665d7b7c442c4d455a9243ab0
Chen Xiaojie <caliburn1994@foxmail.com>
2018-11-21 21:35:37 +0800

    测试版本 2018 11 21  photos

---
+++ assets/js/photos.js
@@ -0,0 +1,3 @@
+document.write(`<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js" type='text/javascript'></script>`);
+document.write(`<link  href="https://unpkg.com/nanogallery2@2.3.0/dist/css/nanogallery2.min.css" rel="stylesheet" type="text/css">`);
+document.write(`<script  type="text/javascript" src="https://unpkg.com/nanogallery2@2.3.0/dist/jquery.nanogallery2.min.js"></script>`);
