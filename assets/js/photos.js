1dcb3cd5f2f6244bd34d2496c631cd61f15ffb22
Chen Xiaojie <caliburn1994@foxmail.com>
2019-01-15 01:24:49 +0800

    - 删除没用的 - 修改样式位置

--- assets/js/photos.js
+++
@@ -1,5 +0,0 @@
-// chrome浏览器不允许使用write语句
-
-// document.write(`<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js" type='text/javascript' ></script>`);
-// document.write(`<link  href="https://unpkg.com/nanogallery2@2.3.0/dist/css/nanogallery2.min.css" rel="stylesheet" type="text/css">`);
-// document.write(`<script  type="text/javascript" src="https://unpkg.com/nanogallery2@2.3.0/dist/jquery.nanogallery2.min.js"></script>`);
