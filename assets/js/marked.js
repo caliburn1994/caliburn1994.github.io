8cbc771e1868213f6cbfc024d069ce380c1d750d
Chen Xiaojie <caliburn1994@foxmail.com>
2020-05-11 16:57:44 +0900

    Update marked.js

--- assets/js/marked.js
+++ assets/js/marked.js
@@ -52,7 +52,7 @@
         sanitizer: null,
         silent: false,
         smartLists: false,
-        smartypants: false,
+        smartypants: true,
         tokenizer: null,
         xhtml: false
       };
@@ -1176,13 +1176,13 @@
 
   function smartypants(text) {
     return text // em-dashes
-    // .replace(/---/g, "\u2014") // en-dashes
-    // .replace(/--/g, "\u2013") // opening singles
-    // .replace(/(^|[-\u2014/(\[{"\s])'/g, "$1\u2018") // closing singles & apostrophes
-    // .replace(/'/g, "\u2019") // opening doubles
-    // .replace(/(^|[-\u2014/(\[{\u2018\s])"/g, "$1\u201C") // closing doubles
-    // .replace(/"/g, "\u201D") // ellipses
-    // .replace(/\.{3}/g, "\u2026");
+    .replace(/---/g, "\u2014") // en-dashes
+    .replace(/--/g, "\u2013") // opening singles
+    .replace(/(^|[-\u2014/(\[{"\s])'/g, "$1\u0027") // closing singles & apostrophes
+    .replace(/'/g, "\u0027") // opening doubles
+    .replace(/(^|[-\u2014/(\[{\u2018\s])"/g, "$1\u0022") // closing doubles
+    .replace(/"/g, "\u0022") // ellipses
+    .replace(/\.{3}/g, "\u2026");
   }
   /**
    * mangle email addresses
