df752b3ce4630ca1cf9de6fce429c4b308f5f40b
Chen Xiaojie <caliburn1994@foxmail.com>
2020-05-24 01:26:20 +0900

    Update smart_quotes.js

--- assets/js/smart_quotes.js
+++ assets/js/smart_quotes.js
@@ -2,7 +2,13 @@
 let id = $('#post-content')
 
 let current_html = id.html()
-    .replace(/，/g, " , ");
+    .replace(/，/g, " , ")
+    .replace(/\^(.+?)\^/g, function (string, first) {
+        // string is the full result of the regex "[2]"
+        //first is the number 2 from "draft [2]"
+        return "<sup>" + (first) + "</sup>";
+    })
+;
 
 id.html(current_html)
 
