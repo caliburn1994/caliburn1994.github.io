58721634c77d95a6a5a8364050bea960d632e72f
kyakya <caliburn1994@gmail.com>
2021-07-18 20:33:47 +0900

    reformat

--- assets/js/post.js
+++ assets/js/post.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
     collapse_codes_blocks()
 
     // 为h2 h3 h4 增加数字
-    insertAutHoeading()
+    insertHeadings()
 
     // 代码块的标签
     build_code_block()
@@ -29,24 +29,24 @@ $(document).ready(function () {
 
 function add_modal(afterId) {
     $("abbr").each(function (index, entry) {
-        let parentAttr = $(this).parent().attr("href");
+        let parentAttr = $(entry).parent().attr("href");
         let href;
         // if href is not exist, the element of that tag is not one we want.
         if (parentAttr) {
             href = parentAttr.replace("#", "")
         } else {
-            $(this).contents().unwrap()
+            $(entry).contents().unwrap()
             return
         }
 
         //add modal
         const html = '<div id="' + href + '" class="modal" style="width:fit-content; width:-webkit-fit-content; width:-moz-fit-content;max-width:50%">' +
-            '<div class="modal-content">' + marked($(this).attr("title")) + " </div>" +
+            '<div class="modal-content">' + marked($(entry).attr("title")) + " </div>" +
             "</div>";
         $(html).insertAfter(afterId)
 
         // css
-        $(this.parentElement).html($(this).text())
+        $(entry.parentElement).html($(entry).text())
             .css("border-bottom", "1px dotted #000")
             .css("text-decoration", "none")
     })
@@ -76,31 +76,32 @@ function add_TOC_title() {
 // </div>
 function collapse_codes_blocks() {
     $('.kyakya_collap').each(function (index, entry) {
-        if ($(this).attr('value') === '') {
-            $(this).text("详情")
+        if ($(entry).attr('value') === '') {
+            $(entry).text("详情")
         } else {
-            $(this).text($(this).attr('value'))
+            $(entry).text($(entry).attr('value'))
         }
 
-        $(this).html(
+        $(entry).html(
             "<details>" +
-                "<summary>" + "<u style='text-decoration-style: wavy;'>" + $(this).html() + "</u>" + "</summary>" +
-                $(this).next().html() +
-            "</details>")
-        $(this).next().remove()
+                "<summary>" + "<u style='text-decoration-style: wavy;'>" + $(entry).html() + "</u>" + "</summary>" +
+                $(entry).next().html() +
+            "</details>"
+        )
+        $(entry).next().remove()
     });
 }
 
 // <h2>heading</h2> ==> <h2>1 heading</h2>
 // insert Auto Numbering for Headings
 // ref :https://stackoverflow.com/questions/5127017/automatic-numbering-of-headings-h1-h6-using-jquery
-function insertAutHoeading() {
-    var indices = [];
+function insertHeadings() {
+    let indices = [];
 
     function addIndex() {
         // jQuery will give all the HNs in document order
         $('h2,h3,h4,h5,h6').each(function (index, entry) {
-            const hIndex = parseInt(this.nodeName.substring(1)) - 2;
+            const hIndex = parseInt(entry.nodeName.substring(1)) - 2;
 
             // just found a levelUp event
             if (indices.length - 1 > hIndex) {
@@ -116,7 +117,7 @@ function insertAutHoeading() {
             indices[hIndex]++;
 
             // display the full position in the hierarchy
-            $(this).prepend(indices.join(".") + ". ");
+            $(entry).prepend(indices.join(".") + ". ");
         });
     }
 
@@ -138,24 +139,24 @@ function build_tags() {
     $(".highlighter-rouge").each(function (index, entry) {
 
         // if the code block is plaintext, do nothing
-        const class_name = $(this).attr("class")
+        const class_name = $(entry).attr("class")
         if (~class_name.indexOf("language-plaintext")) {
             return true;
         }
         // set a name and id for code blocks
-        $(this).attr("name", "code-block-" + index);
-        $(this).attr("id", "code-block-" + index);
+        $(entry).attr("name", "code-block-" + index);
+        $(entry).attr("id", "code-block-" + index);
 
         const code_lang = class_name.split(" ")[0].split("-")[1];
 
         // build tags
         if (next_is_code_block === false) { // build header
-            $(this).before('<ul class="tabs" style="padding-left: 0;line-height: 20px;height: 20px"></ul>');
-            tabs_node = $(this).prev();
+            $(entry).before('<ul class="tabs" style="padding-left: 0;line-height: 20px;height: 20px"></ul>');
+            tabs_node = $(entry).prev();
         }
         tabs_node.append('<li class="tab" style="line-height: 20px;height: 20px" ><a href="#code-block-' + index + '" class="">' + code_lang + '</a></li>');  // build the content
         next_is_code_block = false; //reset
-        const next_node_class_name = $(this).next().attr("class") + "";
+        const next_node_class_name = $(entry).next().attr("class") + "";
         if (~next_node_class_name.indexOf("highlighter-rouge")) {
             next_is_code_block = true;
         }
