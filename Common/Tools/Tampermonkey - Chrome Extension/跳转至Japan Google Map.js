f719cf05356c25d375e51eddf86fa77df3fea3d2
Chen Xiaojie <caliburn1994@gmail.com>
2023-01-29 21:31:38 +0900

    Tampermonkey - Chrome Extension Japan Google Map.js: 去除斜杠

--- Common/Tools/Tampermonkey - Chrome Extension/跳转至Japan Google Map.js
+++ Common/Tools/Tampermonkey - Chrome Extension/跳转至Japan Google Map.js
@@ -4,15 +4,15 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.google.com/maps/*
-// @exclude      https://www.google.com/maps/*hl=ja*
+// @match        https://*.google.com/maps*
+// @exclude      https://*.google.com/maps*hl=ja*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
 // @grant        none
 // ==/UserScript==
 
 (function() {
-console.log('Redirecting Google Map')
-var url = new URL(window.location.href)
-url.searchParams.set('hl', 'ja');
-window.location.replace(url.href);
+    console.log('Redirecting Google Map')
+    var url = new URL(window.location.href)
+    url.searchParams.set('hl', 'ja');
+    window.location.replace(url.href);
 })();
